fca5b995d41a275251a82b19920789aa9226a859
Ehab Ali <ehab.ali.qa@gmail.com>
2020-02-26 12:58:10 +0300

    fix arabic text

--- Shaheen/Models/Trade.cs
+++ Shaheen/Models/Trade.cs
@@ -13,33 +13,41 @@ namespace Shaheen.Models
 
         [Required(ErrorMessage ="*")]
         [StringLength(250)]
+        [Display(Name = "اسم العلامة التجارية")]
         public string TradeName { get; set; }
 
         [StringLength(250)]
+        [Display(Name = "الشركة المصنعة")]
         public string ManufactureCompany { get; set; }
 
         [Required(ErrorMessage = "*")]
         [StringLength(250)]
+        [Display(Name = "معلومات اضافية")]
         public string AdditionalInfo { get; set; }
 
         [Required(ErrorMessage = "*")]
         [StringLength(250)]
+        [Display(Name = "جهة اصدار شهادة الاختبار")]
         public string TestCertificateIssuer { get; set; }
 
         [Required(ErrorMessage = "*")]
         [StringLength(250)]
+        [Display(Name = "رقم التقرير")]
         public string TestReportNumber { get; set; }
 
         [Required(ErrorMessage = "*")]
         [StringLength(250)]
+        [Display(Name = "رقم شهادة الاختبار")]
         public string TestCertificateNumber { get; set; }
 
         [Required(ErrorMessage = "*")]
         [StringLength(250)]
+        [Display(Name = "السجل التجاري للمورد")]
         public string SupplierCommercialRegistry { get; set; }
 
         [Required(ErrorMessage = "*")]
         [StringLength(250)]
+        [Display(Name = "اسم المورد")]
         public string SupplierName { get; set; }
 
     }
